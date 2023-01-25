cd95e30f781f8ab4224db31642b6b3789cf3e905
Mr. Farmer <andrewitzel@web.de>
2023-01-25 15:12:19 +0100

    food quality color cold

--- src/food.py
+++ src/food.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from random import randrange, randint
+from random import randint
 from typing import TYPE_CHECKING
 from cube import Cube
 
@@ -22,5 +22,8 @@ class Food(Cube):
     def __init__(self, game: Game, dirx: int = 1, diry: int = 0, color = (0, 225, 0)) -> None:
         super().__init__((0, 0), game, dirx, diry, color)
 
-        self.quality = randrange(1, 2)
+        self.quality = randint(1, 2)
         self.pos = (randint(1, self._game.cols - 2), randint(1, self._game.rows - 2))
+
+        if self.quality >= 2:
+            self.color = (255, 215, 0)
